d18fa9189c3635ba81fcc20c1ceb0c87ad331543
matin sangabi <sangabiwork@gmail.com>
2023-06-06 12:11:38 -0700

    add server side props posts props

--- src/pages/blogs/index.js
+++ src/pages/blogs/index.js
@@ -1,17 +1,19 @@
 import BlogPage from "@/components/blogs/blogPage";
 import http from "@/services/httpRequest";
 
-
-const Blogs = ({ categories }) => {
-  return <BlogPage categories={categories} />;
+const Blogs = ({ categories, posts }) => {
+  console.log(posts);
+  return <BlogPage categories={categories} posts = {posts} />;
 };
 
 export default Blogs;
 
 export async function getServerSideProps(ctx) {
+  const { data: posts } = await http.get(`posts?page=1&limit=10`);
   const { data: category } = await http.get(`post-category`);
   return {
     props: {
+      posts: posts.data,
       categories: category.data,
     },
   };
